2a9a27c5f91a380c88d2ccb8e3d50ebbcfffb8e7
astnai <aa@agustinarias.com>
2024-06-23 02:54:28 -0300

    first commit

---
+++ src/data/quotes.js
@@ -0,0 +1,117 @@
+export const quotes = [
+  {
+    text: "Your time is limited, so don't waste it living someone else's life.",
+    author: "Steve Jobs",
+    link: "https://en.wikipedia.org/wiki/Steve_Jobs",
+  },
+  {
+    text: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+    link: "https://en.wikipedia.org/wiki/Steve_Jobs",
+  },
+  {
+    text: "Move fast and break things. Unless you are breaking stuff, you are not moving fast enough.",
+    author: "Mark Zuckerberg",
+    link: "https://en.wikipedia.org/wiki/Mark_Zuckerberg",
+  },
+  {
+    text: "Ideas are easy. Implementation is hard.",
+    author: "Guy Kawasaki",
+    link: "https://en.wikipedia.org/wiki/Guy_Kawasaki",
+  },
+  {
+    text: "If you are not embarrassed by the first version of your product, you've launched too late.",
+    author: "Reid Hoffman",
+    link: "https://en.wikipedia.org/wiki/Reid_Hoffman",
+  },
+  {
+    text: "Innovation distinguishes between a leader and a follower.",
+    author: "Steve Jobs",
+    link: "https://en.wikipedia.org/wiki/Steve_Jobs",
+  },
+  {
+    text: "It's not about ideas. It's about making ideas happen.",
+    author: "Scott Belsky",
+    link: "https://en.wikipedia.org/wiki/Scott_Belsky",
+  },
+  {
+    text: "Risk more than others think is safe. Dream more than others think is practical.",
+    author: "Howard Schultz",
+    link: "https://en.wikipedia.org/wiki/Howard_Schultz",
+  },
+  {
+    text: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+    link: "https://en.wikipedia.org/wiki/Walt_Disney",
+  },
+  {
+    text: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
+    author: "Albert Schweitzer",
+    link: "https://en.wikipedia.org/wiki/Albert_Schweitzer",
+  },
+  {
+    text: "Don't be afraid to give up the good to go for the great.",
+    author: "John D. Rockefeller",
+    link: "https://en.wikipedia.org/wiki/John_D._Rockefeller",
+  },
+  {
+    text: "I find that the harder I work, the more luck I seem to have.",
+    author: "Thomas Jefferson",
+    link: "https://en.wikipedia.org/wiki/Thomas_Jefferson",
+  },
+  {
+    text: "Success usually comes to those who are too busy to be looking for it.",
+    author: "Henry David Thoreau",
+    link: "https://en.wikipedia.org/wiki/Henry_David_Thoreau",
+  },
+  {
+    text: "Opportunities don't happen. You create them.",
+    author: "Chris Grosser",
+    link: "https://www.linkedin.com/in/chrisgrosser",
+  },
+  {
+    text: "Try not to become a man of success. Rather become a man of value.",
+    author: "Albert Einstein",
+    link: "https://en.wikipedia.org/wiki/Albert_Einstein",
+  },
+  {
+    text: "Don't watch the clock; do what it does. Keep going.",
+    author: "Sam Levenson",
+    link: "https://en.wikipedia.org/wiki/Sam_Levenson",
+  },
+  {
+    text: "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+    link: "https://en.wikipedia.org/wiki/Eleanor_Roosevelt",
+  },
+  {
+    text: "The only limit to our realization of tomorrow is our doubts of today.",
+    author: "Franklin D. Roosevelt",
+    link: "https://en.wikipedia.org/wiki/Franklin_D._Roosevelt",
+  },
+  {
+    text: "The successful warrior is the average man, with laser-like focus.",
+    author: "Bruce Lee",
+    link: "https://en.wikipedia.org/wiki/Bruce_Lee",
+  },
+  {
+    text: "You miss 100% of the shots you don't take.",
+    author: "Wayne Gretzky",
+    link: "https://en.wikipedia.org/wiki/Wayne_Gretzky",
+  },
+  {
+    text: "Success is walking from failure to failure with no loss of enthusiasm.",
+    author: "Winston Churchill",
+    link: "https://en.wikipedia.org/wiki/Winston_Churchill",
+  },
+  {
+    text: "The only place where success comes before work is in the dictionary.",
+    author: "Vidal Sassoon",
+    link: "https://en.wikipedia.org/wiki/Vidal_Sassoon",
+  },
+  {
+    text: "Success is not in what you have, but who you are.",
+    author: "Bo Bennett",
+    link: "https://en.wikipedia.org/wiki/Bo_Bennett",
+  },
+];
